a5b005885c590c2bf79be568d97e1dd91bedb84e
Gopal Nataraj <gopal.r.nataraj@gmail.com>
2025-02-03 10:04:32 -0800

    added {include,src}/burgers.{hpp,cpp}

---
+++ include/burgers.hpp
@@ -0,0 +1,7 @@
+int inviscidBurgers1d(const double* u_0x,
+                      const double& nt,
+                      const double& nx,
+                      const double& dt,
+                      const double& dx,
+                      const double& tol,
+                      double* u);
---
+++ src/burgers.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <iostream>
+
+#define FAILURE 1
+#define SUCCESS 0
+
+int inviscidBurgers1d(const double* u_0x,
+                      const double& nt,
+                      const double& nx,
+                      const double& dt,
+                      const double& dx,
+                      const double& tol,
+                      double* u) {
+    if ((nx<2)||(nt<2)) return FAILURE;
+    for (int i=0; i<nt; ++i) {
+        std::memcpy(&u[i*nx], u_0x, nx*sizeof(double));
+    }
+    double* uOld = new double[nt*nx];
+    double udiff;
+    do {
+        std::memcpy(uOld, u, nt*nx*sizeof(double));
+        for (int i=0; i<nt; ++i) {
+            for (int j=0; j<nx; ++j) {
+                u[i*nx+j] = ((dx*uOld[(i-1)*nx+j]+dt*pow(uOld[i*nx+j],2.0))/
+                             (dx+dt*(2.0*uOld[i*nx+j]-uOld[i*nx+(j-1)])));
+            }
+        }
+        udiff = 0.0;
+        for (int i=0; i<nt; ++i) {
+            for (int j=0; j<nx; ++j) {
+                udiff += std::abs(u[i*nx+j]-uOld[i*nx+j]);
+            }
+        }
+        std::cout << udiff << "\n";
+    } while (udiff>tol);
+    delete[] uOld;
+    return SUCCESS;
+}

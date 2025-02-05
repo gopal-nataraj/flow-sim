ad39d60205c9ed2419d7b12a0bce25b2af7e1926
Gopal Nataraj <gopal.r.nataraj@gmail.com>
2025-02-04 17:53:06 -0800

    added writeToFile3d() to {include,src}/util.{hpp,cpp}

--- include/util.hpp
+++ include/util.hpp
@@ -7,3 +7,9 @@ int writeToFile2d(const std::string& filename,
                   const int& nt,
                   const int& nx,
                   const double* u);
+
+int writeToFile3d(const std::string& filename,
+                  const int& nt,
+                  const int& ny,
+                  const int& nx,
+                  const double* u);
--- src/util.cpp
+++ src/util.cpp
@@ -17,3 +17,21 @@ int writeToFile2d(const std::string& filename,
     file.close();
     return SUCCESS;
 }
+
+int writeToFile3d(const std::string& filename,
+                  const int& nt,
+                  const int& ny,
+                  const int& nx,
+                  const double* u) {
+    std::ofstream file(filename);
+    for (int i=0; i<nt; ++i) {
+        for (int j=0; j<ny; ++j) {
+            for (int k=0; k<nx-1; ++k) {
+                file << u[i*ny*nx+j*nx+k] << ", ";
+            }
+            file << u[i*ny*nx+j*nx+(nx-1)] << std::endl;
+        }
+    }
+    file.close();
+    return SUCCESS;
+}

63bd70f056194a2ebd882f731908262d954611e6
Gopal Nataraj <gopal.r.nataraj@gmail.com>
2025-02-13 19:49:15 -0800

    added writeInviscidBurgers2d() to python/quiver.py

--- bash/burgers2d.sh
+++ bash/burgers2d.sh
@@ -4,4 +4,7 @@ make
 
 ./bin/burgers 1
 
+python3 ./python/quiver.py
+open ./results/inviscidBurgers2d_quiver.mp4
+
 cd bash
--- python/quiver.py
+++ python/quiver.py
@@ -21,6 +21,26 @@ def writeQuiver2d(filename, u, v, x, y, delay_ms, xticks, yticks):
     quiver_anim = anim.FuncAnimation(fig, updateQuiver, **anim_kwargs)
     quiver_anim.save(filename)
 
+def writeInviscidBurgers2d():
+    filepath_prefix = './results/inviscidBurgers2d_'
+    u_suffix = 'u.txt'
+    v_suffix = 'v.txt'
+    quiver_suffix = 'quiver.mp4'
+    ny = 64
+    d = 2 # decimation factor
+    playback = 0.4 # playback speedup factor
+    u = np.loadtxt(filepath_prefix+u_suffix, delimiter=',')
+    u.shape = (u.shape[0]//ny, ny, u.shape[1])
+    v = np.loadtxt(filepath_prefix+v_suffix, delimiter=',')
+    v.shape = (v.shape[0]//ny, ny, v.shape[1])
+    x = np.linspace(-2.0, 2.0, u.shape[2]//d, endpoint=False)
+    y = np.linspace(-2.0, 2.0, ny//d, endpoint=False)
+    delay_ms = (2000.0-0.0)/(playback*u.shape[0])
+    xticks = [-2.0, 0.0, 2.0]
+    yticks = [-2.0, 0.0, 2.0]
+    writeQuiver2d(filepath_prefix+quiver_suffix, u[:,::d,::d],
+                  v[:,::d,::d], x, y, delay_ms, xticks, yticks)
+
 def writeInviscidBurgers2dExplicit():
     filepath_prefix = './results/inviscidBurgers2dExplicit_'
     u_suffix = 'u.txt'
@@ -42,7 +62,8 @@ def writeInviscidBurgers2dExplicit():
                   v[:,::d,::d], x, y, delay_ms, xticks, yticks)
 
 def main():
-    writeInviscidBurgers2dExplicit()
+    writeInviscidBurgers2d()
+    # writeInviscidBurgers2dExplicit()
 
 if __name__ == '__main__':
     main()
